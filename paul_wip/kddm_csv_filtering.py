345c6de062b6037b6f06240b9af9fc349a3ad3cf
paul_oberauer <oberauer@student.tugraz.at>
2023-06-16 12:55:38 +0200

    fix

--- paul_wip/kddm_csv_filtering.py
+++ paul_wip/kddm_csv_filtering.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import matplotlib.pyplot as plt
 
 input_file = "paul_wip/Undergrad.csv"
 output_file = "paul_wip/Undergrad_filtered.csv"
@@ -15,7 +18,7 @@ def replace_missing_years(df):
             if prev_year is not None and next_year is not None and prev_year == next_year:
                 df.loc[i, "Year"] = next_year
             else:
-                # fix one value is between 2 years 2017 and 2018, set this to 2017 because other features looks good
+                # fix one value is between 2 years 2017 and 2018, set this to 2017 because this State is missing 1 value in 2017
                 df.loc[i, "Year"] = 2017.0
           
     return df
@@ -45,6 +48,120 @@ def replace_state_values(state):
     }
     return state_mapping.get(state, state)
 
+def fixValueMean(df):
+    years = df['Year'].unique()
+    print(years)
+    in_private_mean = []
+    out_private_mean = []
+    in_public_mean = []
+    out_public_mean = []
+    for year in years:
+        #calculate mean In-State
+        filtered_year_data = df[df['Year'] == year]
+        in_private_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'In-State') & (filtered_year_data['Type_1'] == 'Private'), 'Value'].mean(skipna=True))
+        out_private_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'Out-of-State') & (filtered_year_data['Type_1'] == 'Private'), 'Value'].mean(skipna=True))
+        in_public_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'In-State') & (filtered_year_data['Type_1'] == 'Public'), 'Value'].mean(skipna=True))
+        out_public_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'Out-of-State') & (filtered_year_data['Type_1'] == 'Public'), 'Value'].mean(skipna=True))
+
+        #private does not distinguish between in state and out state! 
+        #print(in_private_mean[-1])
+        #print(out_private_mean[-1])
+        #print(in_public_mean[-1])
+
+        #print(out_public_mean[-1])
+        
+        #checkwhere alabama has a missing year value
+        #print("------------------")
+        #unique_counts = filtered_year_data['State'].value_counts()
+        #print("Feature:", year)
+        #print("Value Counts:")
+        #print(unique_counts)
+
+
+    for index, row in df.iterrows():
+            value = row['Value']        
+            
+            if pd.isnull(value):
+                type_1 = row['Type_1']
+                type_2 = row['Type_2']
+                year =  row['Year']
+                index_year = np.where(years == year)[0][0]            
+                if type_1 == "Private" and type_2 == "In-State":
+                        df.at[index, 'Value'] = in_private_mean[index_year]                   
+                elif type_1 == "Private" and type_2 == "Out-of-State":
+                        df.at[index, 'Value'] = out_private_mean[index_year]                     
+                elif type_1 == "Public" and type_2 == "In-State":
+                    df.at[index, 'Value'] = in_public_mean[index_year]                  
+                elif type_1 == "Public" and type_2 == "Out-of-State":
+                        df.at[index, 'Value'] = out_public_mean[index_year]     
+     
+def fixValueInterpolation(df):
+    missing_values_list = []
+    missing_values_df = df[df['Value'].isnull()].copy()
+    for index, row in missing_values_df.iterrows():
+        if pd.isnull(row['Value']):
+            filtered_df = 0
+            if(missing_values_df.at[index, 'Type_1'] == "Private"):
+                filtered_df = df[
+                (df['State'] ==  missing_values_df.at[index, 'State']) &
+                (df['Length'] ==  missing_values_df.at[index, 'Length']) &
+                (df['Expense_1'] ==  missing_values_df.at[index, 'Expense_1']) &
+                (df['Expense_2'] ==  missing_values_df.at[index, 'Expense_2'])
+                ]
+            else:
+                filtered_df = df[
+                (df['State'] ==  missing_values_df.at[index, 'State']) &
+                (df['Length'] ==  missing_values_df.at[index, 'Length']) &
+                (df['Type_1'] ==  missing_values_df.at[index, 'Type_1']) &
+                (df['Type_2'] ==  missing_values_df.at[index, 'Type_2']) &
+                (df['Expense_1'] ==  missing_values_df.at[index, 'Expense_1']) &
+                (df['Expense_2'] ==  missing_values_df.at[index, 'Expense_2'])
+                ]
+                      
+            filtered_df_nan =  filtered_df.dropna(subset=['Value'])
+            values_array =  filtered_df_nan['Value'].astype(float).values
+            years_array = filtered_df_nan['Year'].astype(float).values
+
+            x = np.array(years_array)
+            y = np.array(values_array)
+            
+            slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+           
+            missing_years = filtered_df[filtered_df['Value'].isna()]['Year'].astype(float).values
+            missing_values = slope * missing_years + intercept
+            for i in missing_values:
+                missing_values_list.append(i)
+
+           
+            filtered_df.loc[filtered_df['Value'].isna(), 'Value'] = missing_values
+
+            # create a scatter plot 
+            plt.scatter(x, y, label='Valid Points')
+
+            #plot linear function
+            x_line = np.linspace(0, 2025, 100)
+            y_line = slope * x_line + intercept
+            plt.plot(x_line, y_line, color='green', label='Linear Function')
+            plt.scatter(missing_years, missing_values, color='red', label='Missing Values')
+            plt.xlabel('Year')
+            plt.ylabel('Value')
+            plt.xlim(2012, 2024)
+            plt.ylim(y.min(), y.max())
+            plt.title('Linear Function Estimation')
+            plt.legend()
+
+            # Show the plot
+            #plt.show()
+    #fill in missing values        
+    for index, row in df.iterrows():
+        if pd.isnull(row['Value']):
+            df.at[index, 'Value'] = missing_values_list.pop(0)
+            
+
+    
+    
+     
 # read csv
 print(os.getcwd())
 df = pd.read_csv(input_file)
@@ -67,51 +184,14 @@ df["Value"] = df["Value"].replace(9999999.0, pd.NA)
 df.loc[(df['Expense_1'].isnull()) & (df['Expense_2'] == 'Tuition'), 'Expense_1'] = 'Fees'
 df.loc[(df['Expense_1'].isnull()) & (df['Expense_2'] == 'Board'), 'Expense_1'] = 'Room'
 
-years = df['Year'].unique()
-print(years)
-in_private_mean = []
-out_private_mean = []
-in_public_mean = []
-out_public_mean = []
-for year in years:
-    #calculate mean In-State
-    filtered_year_data = df[df['Year'] == year]
-    in_private_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'In-State') & (filtered_year_data['Type_1'] == 'Private'), 'Value'].mean(skipna=True))
-    out_private_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'Out-of-State') & (filtered_year_data['Type_1'] == 'Private'), 'Value'].mean(skipna=True))
-    in_public_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'In-State') & (filtered_year_data['Type_1'] == 'Public'), 'Value'].mean(skipna=True))
-    out_public_mean.append(filtered_year_data.loc[(filtered_year_data['Type_2'] == 'Out-of-State') & (filtered_year_data['Type_1'] == 'Public'), 'Value'].mean(skipna=True))
-
-    #private does not distinguish between in state and out state! 
-    print(in_private_mean[-1])
-    print(out_private_mean[-1])
-    print(in_public_mean[-1])
-    print(out_public_mean[-1])
-
-
-for index, row in df.iterrows():
-        value = row['Value']        
-        
-        if pd.isnull(value):
-            type_1 = row['Type_1']
-            type_2 = row['Type_2']
-            year =  row['Year']
-            index_year = np.where(years == year)[0][0]            
-            if type_1 == "Private" and type_2 == "In-State":
-                    df.at[index, 'Value'] = in_private_mean[index_year]                   
-            elif type_1 == "Private" and type_2 == "Out-of-State":
-                    df.at[index, 'Value'] = out_private_mean[index_year]                     
-            elif type_1 == "Public" and type_2 == "In-State":
-                df.at[index, 'Value'] = in_public_mean[index_year]                  
-            elif type_1 == "Public" and type_2 == "Out-of-State":
-                    df.at[index, 'Value'] = out_public_mean[index_year]              
-
-
-
+#fixValueMean(df)
+fixValueInterpolation(df)
+         
 # save filtered csv
 df.to_csv(output_file, index=False)
 
-print("----CSV-Filtered-----")
-print(df.info())
+#print("----CSV-Filtered-----")
+#print(df.info())
 for feature in df.columns:
     unique_values = df[feature].unique()
     unique_counts = df[feature].value_counts()
